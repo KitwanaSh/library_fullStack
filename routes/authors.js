45011293db123c58eb4a96882ef7fb07f5534beb
Kitwana Shebabetsi Ezechiel <rodriguekitweze@gmail.com>
2023-06-23 09:40:08 +0200

    Feat: Start the authors pages

---
+++ routes/authors.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.render('authors/index')
+})
+
+// New Author route
+router.get('/new', (req, res) => {
+    res.render('authors/new')
+})
+
+// Create Author route
+router.post('/', (req, res) => {
+    res.send('Create')
+})
+
+module.exports = router
