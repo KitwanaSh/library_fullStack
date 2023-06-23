e9c90a48dda42fb588026868b4cd208d48124e94
Kitwana Shebabetsi Ezechiel <rodriguekitweze@gmail.com>
2023-06-23 14:05:29 +0200

    Fix: Dynamic render

--- routes/authors.js
+++ routes/authors.js
@@ -13,7 +13,20 @@ router.get('/new', (req, res) => {
 
 // Create Author route
 router.post('/', (req, res) => {
-    res.send(req.body.name)
+    const author = new Author({
+        name: req.body.name
+    })
+    author.save((err, newAuthor) => {
+        if(err) {
+            res.render("authors/new", {
+                author: author,
+                errorMessage: 'Error creating Author'
+            })
+        } else {
+            // res.redirect(`authors/${newAuthor.id}`)
+            res.redirect('authors')
+        }
+    })
 })
 
 module.exports = router
