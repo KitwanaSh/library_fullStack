6bf8e5098fba99b3afa8ae692ddd58b2a82b8b3f
Kitwana Shebabetsi Ezechiel <rodriguekitweze@gmail.com>
2023-06-23 13:10:00 +0200

    Feat: Set up the form

--- routes/authors.js
+++ routes/authors.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const Author = require('../models/author')
 
 router.get('/', (req, res) => {
     res.render('authors/index')
@@ -7,7 +8,7 @@ router.get('/', (req, res) => {
 
 // New Author route
 router.get('/new', (req, res) => {
-    res.render('authors/new')
+    res.render('authors/new', { author: new Author() })
 })
 
 // Create Author route
